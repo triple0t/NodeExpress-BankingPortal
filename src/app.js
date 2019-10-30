7fe910df3324e9475ef828b1d33f6f7d5831c3dc
oluwaseun Olorunsola <triple0t1@gmail.com>
2019-10-30 13:01:19 +0100

    feat: Completed Module
    fetch file from json, added other routes

--- src/app.js
+++ src/app.js
@@ -6,14 +6,37 @@ const app = express();
 
 const port = 3000;
 
-app.set('views', path.join(__dirname, 'views'));
+const pathToViewsDir = path.join(__dirname, 'views');
+const pathToPublicDir = path.join(__dirname, 'public');
+const pathToJsonDir = path.join(__dirname, 'json');
+
+app.set('views', pathToViewsDir);
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(pathToPublicDir));
 
-app.get('/', (req, res) => {
-  return res.render('index', {title: 'Hello World'});
-});
+const accountData = fs.readFileSync(path.join(pathToJsonDir, 'accounts.json'), {encoding: 'utf8'});
+const userData = fs.readFileSync(path.join(pathToJsonDir, 'users.json'), {encoding: 'utf8'});
+
+// PluralSight will not pass the tests with the try/ catch block included
+// try {
+  const accounts = JSON.parse(accountData);
+  const users = JSON.parse(userData);
+
+  app.get('/', (req, res) => {
+    return res.render('index', {title: 'Account Summary', accounts});
+  });
+
+  app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+  app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+  app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+  app.get('/profile', (req, res) => res.render('profile', { user: users[0] }))
+
+// } catch (error) {
+//   console.error('Error Parsing File: ', error);
+//   throw error;
+// }
 
 app.listen(port, () => {
   console.log(`App Server Started on: http://localhost:${port}`);
